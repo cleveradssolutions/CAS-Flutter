6c191c33908611d0cad549ca3e6624b51610ff14
Dmytro Uzhva <kraativ@gmail.com>
2025-01-16 11:54:57 +0200

    Set plugin version 0.7.6, update CAS version to 3.9.9

--- tools/version_updater.py
+++ tools/version_updater.py
@@ -4,7 +4,7 @@ import sys
 
 
 # To run, execute the command:
-# python version_updater.py CAS_VERSION=3.9.8 FLUTTER_PLUGIN_VERSION=0.7.5
+# python version_updater.py CAS_VERSION=3.9.9 FLUTTER_PLUGIN_VERSION=0.7.6
 
 def parse_args(args):
     cas_version = None
