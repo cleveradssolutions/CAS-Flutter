cb19b50ebd1a42f7c62987eb88760d56b58a43b1
d.eshenko <d.eshenko@psvgamestudio.com>
2025-08-08 14:29:26 +0300

    Update example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 const String _casId = 'demo';
+const bool _useAutoLoad = true;
 
 void main() {
   runApp(const MyApp());
@@ -36,40 +37,48 @@ class _HomeScreenState extends State<HomeScreen> {
   String? _sdkVersion;
 
   final OnAdImpressionListener _impressionListener = OnAdImpressionListener(
-      (ad) async => logDebug(
-          '${await ad.getFormat()} ad impression: ${await ad.getSourceName()}'));
+      (ad) async => {
+            logDebug(
+                '${await ad.getFormat()} ad impression: ${await ad.getSourceName()}')
+          });
 
   final _bannerListener = AdViewListener(
-    onAdViewLoaded: () => logDebug('Banner ad loaded!'),
-    onAdViewFailed: (message) => logDebug('Banner failed! $message'),
-    onAdViewClicked: () => logDebug('Banner ad clicked!'),
+    onAdViewLoaded: () => {logDebug('Banner ad loaded!')},
+    onAdViewFailed: (message) => {logDebug('Banner failed! $message')},
+    onAdViewClicked: () => {logDebug('Banner ad clicked!')},
   );
 
   final ScreenAdContentCallback _contentCallback = ScreenAdContentCallback(
-    onAdLoaded: (ad) async => logDebug(
-        '${await ad.getFormat()} ad loaded: ${await ad.getSourceName()}'),
+    onAdLoaded: (ad) async => {
+      logDebug('${await ad.getFormat()} ad loaded: ${await ad.getSourceName()}')
+    },
     onAdFailedToLoad: (format, error) =>
-        logDebug('$format ad failed to load: $error'),
-    onAdShowed: (ad) async => logDebug(
-        '${await ad.getFormat()} ad showed: ${await ad.getSourceName()}'),
+        {logDebug('$format ad failed to load: $error')},
+    onAdShowed: (ad) async => {
+      logDebug('${await ad.getFormat()} ad showed: ${await ad.getSourceName()}')
+    },
     onAdFailedToShow: (format, error) =>
-        logDebug('$format ad failed to show: $error'),
-    onAdClicked: (ad) async => logDebug(
-        '${await ad.getFormat()} ad clicked: ${await ad.getSourceName()}'),
-    onAdDismissed: (ad) async => logDebug(
-        '${await ad.getFormat()} ad dismissed: ${await ad.getSourceName()}'),
+        {logDebug('$format ad failed to show: $error')},
+    onAdClicked: (ad) async => {
+      logDebug(
+          '${await ad.getFormat()} ad clicked: ${await ad.getSourceName()}')
+    },
+    onAdDismissed: (ad) async => {
+      logDebug(
+          '${await ad.getFormat()} ad dismissed: ${await ad.getSourceName()}')
+    },
   );
 
   @override
   void initState() {
     super.initState();
-    _initializeCAS();
+    _initAdsSDK();
 
-    _loadAppOpenAd();
-    _loadInterstitialAd();
-    _loadRewardedAd();
+    _initAppOpenAd();
+    _initInterstitialAd();
+    _initRewardedAd();
 
-    _getNativeSDKVersion();
+    _getAdsSDKVersion();
   }
 
   @override
@@ -123,6 +132,10 @@ class _HomeScreenState extends State<HomeScreen> {
                     onPressed: _showRewarded,
                     child: const Text('Show Rewarded'),
                   ),
+                  ElevatedButton(
+                    onPressed: _showConsentFlowIfRequired,
+                    child: const Text('Show Consent Flow if required'),
+                  ),
                 ],
               ),
             ),
@@ -132,7 +145,7 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
-  void _initializeCAS() {
+  void _initAdsSDK() {
     // Set Ads Settings
     CAS.settings.setDebugMode(true);
     CAS.settings.setTaggedAudience(Audience.notChildren);
@@ -152,7 +165,7 @@ class _HomeScreenState extends State<HomeScreen> {
         .build();
   }
 
-  void _getNativeSDKVersion() async {
+  void _getAdsSDKVersion() async {
     final String sdkVersion = await CAS.getSDKVersion();
     setState(() {
       _sdkVersion = sdkVersion;
@@ -183,34 +196,43 @@ class _HomeScreenState extends State<HomeScreen> {
       casId: _casId,
       adListener: _bannerListener,
       onImpressionListener: _impressionListener,
+      // AdSize.banner by default
       size: AdSize.getAdaptiveBanner(maxWidth),
+      // true by default
       isAutoloadEnabled: true,
+      // 30 sec by default
       refreshInterval: 30,
     );
   }
 
-  void _loadAppOpenAd() {
+  void _initAppOpenAd() {
     _appOpen.contentCallback = _contentCallback;
     _appOpen.impressionListener = _impressionListener;
-    _appOpen.setAutoloadEnabled(false); // by default
-    _appOpen.setAutoshowEnabled(false); // by default
-    _appOpen.load(); // if autoload disabled
+    _appOpen.setAutoloadEnabled(_useAutoLoad); // false by default
+    _appOpen.setAutoshowEnabled(true); // false by default
+    if (!_useAutoLoad) {
+      _appOpen.load();
+    }
   }
 
   void _showAppOpen() async {
     _appOpen.show();
   }
 
-  void _loadInterstitialAd() {
+  void _initInterstitialAd() {
     _interstitial.contentCallback = _contentCallback;
     _interstitial.impressionListener = _impressionListener;
-    _interstitial.setAutoloadEnabled(false); // by default
-    _interstitial.setAutoshowEnabled(false); // by default
+    _interstitial.setAutoloadEnabled(_useAutoLoad); // false by default
+    _interstitial.setAutoshowEnabled(true); // false by default
     _interstitial.setMinInterval(0); // by default
-    _interstitial.load(); // if autoload disabled
+    if (!_useAutoLoad) {
+      _interstitial.load();
+    }
   }
 
   void _showInterstitial() async {
+    // You can show ads without checking for isLoaded,
+    // expecting an error in onAdFailedToShow.
     if (await _interstitial.isLoaded()) {
       _interstitial.show();
     } else {
@@ -218,15 +240,19 @@ class _HomeScreenState extends State<HomeScreen> {
     }
   }
 
-  void _loadRewardedAd() {
+  void _initRewardedAd() {
     _rewarded.contentCallback = _contentCallback;
     _rewarded.impressionListener = _impressionListener;
-    _rewarded.setExtraFillInterstitialAdEnabled(true); // by default
-    _rewarded.setAutoloadEnabled(false); // by default
-    _rewarded.load(); // if autoload disabled
+    _rewarded.setExtraFillInterstitialAdEnabled(true); // true by default
+    _rewarded.setAutoloadEnabled(_useAutoLoad); // false by default
+    if (!_useAutoLoad) {
+      _rewarded.load();
+    }
   }
 
   void _showRewarded() async {
+    // You can show ads without checking for isLoaded,
+    // expecting an error in onAdFailedToShow.
     if (await _rewarded.isLoaded()) {
       _rewarded.show(OnRewardEarnedListener((ad) async {
         logDebug('Rewarded ad earned reward: ${await ad.getSourceName()}');
@@ -234,6 +260,13 @@ class _HomeScreenState extends State<HomeScreen> {
     } else {
       logDebug('Rewarded ad not ready to show');
     }
+  }
+
+  void _showConsentFlowIfRequired() {
+    ConsentFlow.create()
+        .setOnDismissCallback(_onConsentFlowDismissed)
+        .withPrivacyPolicy('https://example.com/')
+        .showIfRequired();
   }
 }
 
